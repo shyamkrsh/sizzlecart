2c1b4c0f136268f9155b36adca03614236737e7d
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2024-12-26 17:46:47 +0530

    category added

--- frontend/src/Component/Home/CategoryCard.jsx
+++ frontend/src/Component/Home/CategoryCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CategoryCard() {
   return (
     <div className='flex flex-col items-center cursor-pointer shadow-md p-5 bg-white rounded-md m-3'>
-        <img src="src/assets/img1.jpg" alt="" className='w-[5rem] h-[5rem]' />
+        <img src="https://i.ibb.co/k6dZwJr/img1.jpg" alt="" className='w-[5rem] h-[5rem]' />
         <p className=' text-slate-800 font-semibold' style={{fontSize: '1rem'}}>Grocery</p>
     </div>
   )
