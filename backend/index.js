48ec24c067075b2b1141b223132db27aa4b6460e
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2024-12-31 10:25:45 +0530

    origin added

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ async function main() {
 }
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://sizzlecartbackend.vercel.app"],
     credentials: true,
     methods: "GET, POST, PUT, DELETE"
 
