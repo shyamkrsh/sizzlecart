f4bc1dfe5ace090ac5a1291eef68224d19ad769f
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-01-08 18:31:57 +0530

    error fixed

--- frontend/src/Component/Home/TrendingProducts.jsx
+++ frontend/src/Component/Home/TrendingProducts.jsx
@@ -6,7 +6,7 @@ function TrendingProducts() {
     const [products, setProducts] = React.useState([]);
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/products/trending`).then((res) => {
-            setProducts(res.data);
+            setProducts(res?.data);
         })
     }, [])
     
@@ -15,9 +15,9 @@ function TrendingProducts() {
             <h1 className='text-3xl md:text-4xl text-center'>𝐓𝐫𝐞𝐧𝐝𝐢𝐧𝐠 𝐏𝐫𝐨𝐝𝐮𝐜𝐭𝐬</h1>
             <div className='card-container px-[5%] md:px-[10%] grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-12 place-items-center'>
                 {
-                    products?.map((product, index) => {
-                        return <Product key={index} image={product?.thumbnail} title={product?.name} price={product?.price} offers={"20% off"} weight={product?.weight} deliveryBy={"Delivery by Sat Dec 28 | ₹40Free"}/>
-                    })
+                    products?.map((product, index) => (
+                        <Product key={index} image={product?.thumbnail} title={product?.name} price={product?.price} offers={"20% off"} weight={product?.weight} deliveryBy={"Delivery by Sat Dec 28 | ₹40Free"}/>
+                    ))
                 }
             </div>
         </div>
