0e6af11cf0afa1a9fd3d3bca1d71e50f08316808
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-01-16 21:29:14 +0530

    ShowProduct page designed

--- frontend/src/Component/SearchProducts/Searchbar.jsx
+++ frontend/src/Component/SearchProducts/Searchbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
+import { BsCart3 } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import Badge from '@mui/material/Badge';
 
-function Searchbar({setValue, value}) {
-    
+function Searchbar({ setValue, value }) {
+
     const navigate = useNavigate();
+    const [productsCount, setProductsCount] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            const products = JSON.parse(localStorage.getItem('products'));
+            setProductsCount(products?.length);
+        })
+    }, []);
 
     const handleChange = (event) => {
         setValue(event.target.value);
@@ -12,8 +24,14 @@ function Searchbar({setValue, value}) {
 
     return (
         <div className='flex items-center justify-start gap-5 w-full fixed top-0 left-0 z-50 bg-slate-50 h-14 px-5 shadow-md'>
-            <FaArrowLeftLong className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/")}/>
-            <input type="text" placeholder='Search for products...' className='bg-transparent text-slate-800 outline-none h-full flex-1 ' value={value} onChange={handleChange} style={{fontSize: '1.2rem'}} />
+            <FaArrowLeftLong className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/")} />
+            <input type="text" placeholder='Search for products...' className='bg-transparent text-slate-800 outline-none h-full flex-1 ' value={value} onChange={handleChange} style={{ fontSize: '1.2rem' }} />
+            <div className='flex gap-5 items-center'>
+                <Badge badgeContent={productsCount} color="error" >
+                    <BsCart3 className='text-2xl text-slate-600' />
+                </Badge>
+                <FaRegUserCircle className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/user-profile")} />
+            </div>
         </div>
     )
 }
--- frontend/src/Component/ShowProduct/ShowProductPage.jsx
+++ frontend/src/Component/ShowProduct/ShowProductPage.jsx
@@ -8,6 +8,7 @@ import { BsFillStarFill } from "react-icons/bs";
 import { BsCart3 } from "react-icons/bs";
 import { FaRegUserCircle } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
+import Badge from '@mui/material/Badge';
 
 function ShowProductPage() {
   const { id } = useParams();
@@ -15,7 +16,15 @@ function ShowProductPage() {
   const navigate = useNavigate();
   const [displayImage, setDisplayImage] = useState(product?.thumbnail);
   const [showIndex, setShowIndex] = useState(0);
-  
+  const [productsCount, setProductsCount] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      const products = JSON.parse(localStorage.getItem('products'));
+      setProductsCount(products?.length);
+    })
+  }, []);
+
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/products/${id}`).then((res) => {
       setProduct(res?.data?.data);
@@ -27,16 +36,18 @@ function ShowProductPage() {
   }, [])
 
 
-  let style = `flex flex-col items-center cursor-pointer shadow-lg p-2 bg-white rounded-lg m-3 w-[4rem] h-[4rem] mx-auto `
-  
+  let style = `flex flex-col items-center cursor-pointer shadow-lg p-2 bg-white rounded-lg w-[4rem] h-[4rem]`
+
   return (
     <div className='pb-20'>
-      <div className='flex items-center justify-start gap-5 w-full fixed top-0 left-0 z-50 bg-slate-50 h-14 px-5 shadow-md'>
+      <div className='flex items-center justify-between gap-5 w-full fixed top-0 left-0 z-50 bg-slate-50 h-14 px-5 shadow-md'>
         <FaArrowLeftLong className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/")} />
-        <div className='flex gap-2 items-center'>
-          <CiSearch className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/")} />
-          <BsCart3 className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/")} />
-          <FaRegUserCircle className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/")} />
+        <div className='flex gap-5 items-center'>
+          <CiSearch className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/search-products")} />
+          <Badge badgeContent={productsCount} color="error"  onClick={() => navigate("/cartPage")}>
+            <BsCart3 className='text-2xl text-slate-600' />
+          </Badge>
+          <FaRegUserCircle className='text-2xl text-slate-600 cursor-pointer' onClick={() => navigate("/profilePage")} />
         </div>
       </div>
       <div className='w-[100%] px-3 flex items-center justify-start gap-2 border-b-2 py-1'>
@@ -52,17 +63,17 @@ function ShowProductPage() {
         <img src={displayImage} className='w-[95%] h-[13rem] rounded-md' />
       </div>
 
-      <div className='flex items-center justify-center'>
-        <div className={showIndex == 0 ? `${style} border-2 border-blue-300` :  `${style}`} onClick={() => {setShowIndex(0), setDisplayImage(product?.images[1])}} >
+      <div className='w-[100%] flex items-center justify-center gap-3'>
+        <div className={showIndex == 0 ? `${style} border-2 border-blue-300` : `${style}`} onClick={() => { setShowIndex(0), setDisplayImage(product?.images[1]) }} >
           <img src="https://i.ibb.co/k6dZwJr/img1.jpg" alt="" className='w-[3rem] h-[3rem]' />
         </div>
-        <div className={showIndex == 1 ? `${style} border-2 border-blue-300` :  `${style}`} onClick={() => {setShowIndex(1), setDisplayImage(product?.images[0])}}>
+        <div className={showIndex == 1 ? `${style} border-2 border-blue-300` : `${style}`} onClick={() => { setShowIndex(1), setDisplayImage(product?.images[0]) }}>
           <img src="https://i.ibb.co/k6dZwJr/img1.jpg" alt="" className='w-[3rem] h-[3rem]' />
         </div>
-        <div className={showIndex == 2 ? `${style} border-2 border-blue-300` :  `${style}`} onClick={() => {setShowIndex(2), setDisplayImage(product?.images[1])}}>
+        <div className={showIndex == 2 ? `${style} border-2 border-blue-300` : `${style}`} onClick={() => { setShowIndex(2), setDisplayImage(product?.images[1]) }}>
           <img src="https://i.ibb.co/k6dZwJr/img1.jpg" alt="" className='w-[3rem] h-[3rem]' />
         </div>
-        <div className={showIndex == 3 ? `${style} border-2 border-blue-300` :  `${style}`} onClick={() => {setShowIndex(3), setDisplayImage(product?.images[1])}}>
+        <div className={showIndex == 3 ? `${style} border-2 border-blue-300` : `${style}`} onClick={() => { setShowIndex(3), setDisplayImage(product?.images[0]) }}>
           <img src="https://i.ibb.co/k6dZwJr/img1.jpg" alt="" className='w-[3rem] h-[3rem]' />
         </div>
       </div>
@@ -79,23 +90,17 @@ function ShowProductPage() {
             ))
             }
           </p>
-          <p className='text-slate-900 text-sm -mt-1'>{product?.deliveryBy}</p>
+          <p className='text-slate-900 text-sm -mt-1'>{product?.description}</p>
         </div>
       </div>
 
       <div className='review px-[5%] mt-10'>
         <h1 className='text-xl font-semibold py-3'>Reviews & Ratings</h1>
-
-
         <div>
-
           <ReviewCard image={"https://i.ibb.co/Zm6qmB9/user.png"} username={"shashisharma"} ratings={4} content={"This very tasty spice i love it."} />
           <ReviewCard image={"https://i.ibb.co/Zm6qmB9/user.png"} username={"shashisharma"} ratings={4} content={"This very tasty spice i love it."} />
-
         </div>
-
       </div>
-
     </div >
   )
 }
