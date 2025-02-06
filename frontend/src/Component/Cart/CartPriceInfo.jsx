834257afcd1b470e29481817e51d2e6c8e37f091
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-06 15:05:31 +0530

    price updated

--- frontend/src/Component/Cart/CartPriceInfo.jsx
+++ frontend/src/Component/Cart/CartPriceInfo.jsx
@@ -29,30 +29,30 @@ function CartPriceInfo() {
       <div>
         <p className='flex justify-between px-5 py-2'>
           <p className=' text-slate-600' style={{ fontWeight: '600', fontSize: '1rem' }}>Price (2 items)</p>
-          <p className='  text-slate-600' style={{ fontWeight: '600', fontSize: '1rem' }}>₹91,993</p>
+          <p className='  text-slate-600' style={{ fontWeight: '600', fontSize: '1rem' }}>₹ {totalPrice}</p>
         </p>
         <p className='flex justify-between px-5 py-2'>
           <p className=' text-slate-600' style={{ fontWeight: '600', fontSize: '1rem' }}>Discount</p>
-          <p className=' text-green-600' style={{ fontWeight: '600', fontSize: '1rem' }}> - ₹37,993</p>
+          <p className=' text-green-600' style={{ fontWeight: '600', fontSize: '1rem' }}> - ₹ {totalPrice * 5 / 100}</p>
         </p>
         <p className='flex justify-between px-5 py-2'>
           <p className=' text-slate-600' style={{ fontWeight: '600', fontSize: '1rem' }}>Deliver Charges</p>
-          <p className=' text-green-600' style={{ fontWeight: '600', fontSize: '1rem' }}> ₹280Free</p>
+          <p className=' text-green-600' style={{ fontWeight: '600', fontSize: '1rem' }}> Free</p>
         </p>
         <p className='flex justify-between px-5 py-2'>
           <p className=' text-slate-600' style={{ fontWeight: '600', fontSize: '1rem' }}>Secured Packaging Fee</p>
-          <p className=' text-green-600' style={{ fontWeight: '600', fontSize: '1rem' }}> ₹275</p>
+          <p className=' text-green-600' style={{ fontWeight: '600', fontSize: '1rem' }}>+ ₹ 10</p>
         </p>
       </div>
       <div className='flex items-center justify-between px-5 py-3 mt-5 ' style={{ borderTop: '1px dashed #d3dbd5', borderBottom: '1px dashed #d3dbd5' }}>
         <h2 className='text-xl font-semibold'>Total Amount</h2>
-        <h2 className='text-xl font-semibold'>₹{totalPrice}</h2>
+        <h2 className='text-xl font-semibold'>₹ {totalPrice - (totalPrice * 5 / 100) + 10}</h2>
       </div>
       <div>
-        <p className='text-green-600 font-semibold px-5 py-5'>You will save ₹{totalPrice - totalPrice*(0.2)} on this order</p>
+        <p className='text-green-600 font-semibold px-5 py-5'>You will save ₹ {(totalPrice * 5 / 100) - 10} on this order</p>
       </div>
       <div className='w-[100%] h-[4rem] px-5 bg-white flex justify-between items-center md:hidden'>
-        <h2 className='text-2xl font-semibold'>₹{totalPrice - totalPrice*(0.2)}</h2>
+        <h2 className='text-2xl font-semibold'>₹{totalPrice - (totalPrice * 5 / 100) + 10}</h2>
         <button className='text-white px-12 py-3 font-semibold' style={{ backgroundColor: '#fb641b' }}>PLACE ORDER</button>
       </div>
     </div>
